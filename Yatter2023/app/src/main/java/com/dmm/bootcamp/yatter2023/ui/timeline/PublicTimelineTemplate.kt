f09049839b24133be6a154342447ac08ab3adde4
Koyuki Izumi <g1920506@is.ocha.ac.jp>
2023-07-28 12:25:16 +0900

    update: アイコン画像, テーマカラー

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
