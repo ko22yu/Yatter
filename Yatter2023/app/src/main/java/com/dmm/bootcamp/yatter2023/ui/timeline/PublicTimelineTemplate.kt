5e3c9d79b76a70bd5068a1abfcba4103aa3a4468
Koyuki Izumi <g1920506@is.ocha.ac.jp>
2023-07-28 11:13:56 +0900

    add: ダイアログの表示

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
@@ -1,6 +1,7 @@
 package com.dmm.bootcamp.yatter2023.ui.timeline
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,18 +11,26 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,12 +48,24 @@ fun PublicTimelineTemplate(
     onRefresh: () -> Unit,
 ) {
     val pullRefreshState = rememberPullRefreshState(isRefreshing, onRefresh)
+    var showDialog by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text(text = "タイムライン")
                 },
+                actions = {
+                    IconButton(onClick = {
+                        showDialog = true
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = "profile"
+                        )
+                    }
+                }
             )
         },
         floatingActionButton = {
@@ -82,6 +103,9 @@ fun PublicTimelineTemplate(
             }
         }
     }
+    if (showDialog) {
+        showDialog = Dialog()
+    }
 }
 
 @Preview
@@ -107,4 +131,39 @@ private fun PublicTimelineTemplatePreview() {
             )
         }
     }
+}
+
+@Composable
+fun Dialog(): Boolean {
+    var showDialog by remember { mutableStateOf(true) }
+    var result by remember { mutableStateOf("Result") }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                result = "Dismiss"
+                showDialog = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        result = "OK"
+                        showDialog = false
+                    }
+                ) {
+                    Text("閉じる")
+                }
+            },
+            title = {
+                Text("アプリの使い方")
+            },
+            text = {
+                Column() {
+                    Text("タイムライン画面では他の人のつぶやきを見ることができます。")
+                    Text("右下のボタンを押すことで自由につぶやくことができます。")
+                }
+            },
+        )
+    }
+    return showDialog
 }
