9a8a510a399a261ae4ac5d4997d4f644e1dedb08
Koyuki Izumi <g1920506@is.ocha.ac.jp>
2023-07-28 15:03:17 +0900

    fix: M3

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginTemplate.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginTemplate.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
@@ -35,83 +35,85 @@ fun LoginTemplate(
     onClickLogin: () -> Unit,
     onClickRegister: () -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "ログイン")
-                }
-            )
-        }
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-                .padding(8.dp),
-        ) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    text = "ユーザー名"
+    Yatter2023Theme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "ログイン")
+                    }
                 )
-                OutlinedTextField(
-                    singleLine = true,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp),
-                    value = userName,
-                    onValueChange = onChangedUserName,
-                    placeholder = {
-                        Text(text = "username")
-                    },
-                )
-
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "パスワード"
-                )
-                OutlinedTextField(
-                    singleLine = true,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp),
-                    value = password,
-                    onValueChange = onChangedPassword,
-                    placeholder = {
-                        Text(text = "password")
-                    },
-                )
-
-                Button(
-                    enabled = isEnableLogin,
-                    onClick = onClickLogin,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                ) {
-                    Text(text = "ログイン")
-                }
-
-                Divider(modifier = Modifier.padding(vertical = 16.dp))
-
-                Text(
-                    text = "はじめてご利用の方は",
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.body2
-                )
-                TextButton(
-                    onClick = onClickRegister,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = "新規会員登録")
-                }
             }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+                    .padding(8.dp),
+            ) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        text = "ユーザー名"
+                    )
+                    OutlinedTextField(
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        value = userName,
+                        onValueChange = onChangedUserName,
+                        placeholder = {
+                            Text(text = "username")
+                        },
+                    )
 
-            if (isLoading) {
-                CircularProgressIndicator()
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "パスワード"
+                    )
+                    OutlinedTextField(
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        value = password,
+                        onValueChange = onChangedPassword,
+                        placeholder = {
+                            Text(text = "password")
+                        },
+                    )
+
+                    Button(
+                        enabled = isEnableLogin,
+                        onClick = onClickLogin,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                    ) {
+                        Text(text = "ログイン")
+                    }
+
+                    Divider(modifier = Modifier.padding(vertical = 16.dp))
+
+                    Text(
+                        text = "はじめてご利用の方は",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    TextButton(
+                        onClick = onClickRegister,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "新規会員登録")
+                    }
+                }
+
+                if (isLoading) {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
